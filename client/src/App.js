b5871437cc96513b87a0e59e919ce5aedd9d1d53
John-Pena <jmp1495@gmail.com>
2022-06-22 23:16:32 -0500

    loaded snapshot code from end of lesson 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -18,13 +23,13 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
+      <div className="flex-column justify-flex-start min-100-vh">
         <Header />
-        <div className='container'>
-        <Home />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
     </ApolloProvider>
   );
 }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_THOUGHTS = gql`
-  query thoughtd($username: String) {
+  query thoughts($username: String) {
     thoughts(username: $username) {
       _id
       thoughtText
